1ce550427bbde13b757b8e155e68ef57e4b25c0b
Douglas Chifetete <dschifetete@gmail.com>
2021-07-25 18:23:55 +0200

    Project setup

---
+++ src/test/java/com/douglaschifetete/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglaschifetete.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
